3dac23ca85da89ce95206b397e1269eb9013a480
Cory Tomlinson <corytomlinson6640@gmail.com>
2024-01-23 15:36:31 -0700

    remove unnecessary comment

--- src/com/github/ptlux1517/leetcode/LeetCodeRunner.java
+++ src/com/github/ptlux1517/leetcode/LeetCodeRunner.java
@@ -22,7 +22,7 @@ public class LeetCodeRunner {
          case 0 -> new P0_FizzBuzz().run();
          case 1 -> new P1_Two_Sum().run();
          case 2 -> new P2_Add_Two_Numbers().run();
-         case 3 -> new P3_Longest_Substring_Without_Repeating_Characters().run(); //longest substring without repeating characters
+         case 3 -> new P3_Longest_Substring_Without_Repeating_Characters().run();
          case 4 -> new P4_Median_Of_Two_Sorted_Arrays().run();
          case 9 -> new P9_Palindrome_Number().run();
          case 13 -> new P13_Roman_To_Integer().run();
@@ -33,35 +33,6 @@ public class LeetCodeRunner {
       };
 
       System.out.println("\nExecution took "+runTime.toMillis()+"."+String.format("%03d",runTime.toNanosPart()/1000)+" ms");
-
-//      String s = "";
-//      boolean p = isPalindrome(s);
-//      System.out.printf("\nIs (%s) a palindrome? %b\n",s,p);
-   }
-
-   private static boolean isPalindrome(String s) {
-      if (s==null) return false;
-      if (s.length()==0) return true;
-      boolean isPalindrome = true;
-      for (int l=0, r=s.length()-1; l<=r; l++, r--) {
-         if (s.charAt(l)!=s.charAt(r)) {
-            isPalindrome = false;
-            break;
-         }
-      }
-      return isPalindrome;
-   }
-
-   /**
-    *
-    * @return idx of target elem, or -1 if not present
-    */
-   private static int binarySearch(int[] arr, int lIdx, int rIdx, int target) {
-      if (lIdx>rIdx) return -1;
-      int mid = lIdx + (rIdx-lIdx)/2;
-      if (arr[mid]>target) return binarySearch(arr,lIdx,mid-1,target);
-      if (arr[mid]<target) return binarySearch(arr,mid+1,rIdx,target);
-      return mid;
    }
 }
 
